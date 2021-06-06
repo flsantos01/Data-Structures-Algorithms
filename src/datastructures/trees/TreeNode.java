62266d377f36ece370f10fe67cc5102c5c87a7f4
flsantos01 <flsantos32x@outlook.com>
2021-06-05 23:39:17 -0300

    TreeNode class update

--- src/datastructures/trees/TreeNode.java
+++ src/datastructures/trees/TreeNode.java
@@ -2,12 +2,29 @@ package datastructures.trees;
 
 import datastructures.general.Node;
 
-public class TreeNode<T> extends Node<T>{
+public class TreeNode<T> extends Node<T> {
+
+	private TreeNode<T> left;
+	private TreeNode<T> right;
 
 	protected TreeNode(T value) {
 		super(value);
 	}
-	
-	
-	
+
+	public TreeNode<T> getLeft() {
+		return left;
+	}
+
+	public void setLeft(TreeNode<T> left) {
+		this.left = left;
+	}
+
+	public TreeNode<T> getRight() {
+		return right;
+	}
+
+	public void setRight(TreeNode<T> right) {
+		this.right = right;
+	}
+
 }
